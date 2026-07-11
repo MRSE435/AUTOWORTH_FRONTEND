b3a8a6eefa5c2994c9056df1e627ed9ef7e32670
MRSE435 <m54655967@gmail.com>
2026-07-11 23:21:48 +0530

    Created a form component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 import Navbar from "./components/Navbar.jsx";
-export default function Home() {
-  return (
-      <>
-        <Navbar />
+import Normalform from "./components/Normalform";
 
-      </>
-  );
+export default function Home() {
+    return (
+        <>
+            <Navbar/>
+            <div className="w-full h-screen flex ">
+                <div className="w-2/10 bg-[#2B2B2B] p-6 ">
+                    <Normalform/>
+                </div>
+
+                <div className="flex-1 bg-red-600">
+
+                </div>
+            </div>
+        </>
+    );
 }
